e4ea52a98ab3fa0b82be768315d5cd26116406a5
Hanif Ibrahim Syuaib <hanifsyuaib@gmail.com>
2024-07-25 14:32:31 +0700

    Update sonarcloud

--- project_matrix/settings.py
+++ project_matrix/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: Only put your backend and frontend domain in production
 ALLOWED_HOSTS = [os.environ.get("FRONTEND_DOMAIN"), os.environ.get("BACKEND_DOMAIN")] 
